4945e69d34fe0e46a24fcc19e83e08b43193b655
Alejandro Gonzalez Sole <guatedude@gmail.com>
2015-03-27 19:25:12 -0400

    Fixed missing init in sample app. Update now renames pixi-dev.js to pixi.js

--- template/src/index.js
+++ template/src/index.js
@@ -1,20 +1,17 @@
 import PIXI from 'pixi';
-import PIXIHelper from 'pixi-helper';
+import GameWindow from 'game-window';
 import Assets from './assets';
 import MainScene from './scenes/main';
-
-//Create a new stage
-var stage = new PIXIHelper.Stage({
-  backgroundColor: 0x66FF99,
-  screenSize:{
-    width: 800,
-    height: 600
-  }
+//Create a new game window
+GameWindow.init({
+  screenWidth: 1334,
+  screenHeight: 750,
+  scale: true
 });
 
 //Wait for assets to be loaded
 Assets.preload(function (){
   //create a new scene and set the current stage
   var scene = new MainScene();
-  stage.setScene(scene);
+  GameWindow.setScene(scene);
 });
